7920c08819ee06e11f9cb0406e6d0d001b0c77eb
Noah <noah.ntabo@student.moringaschool.com>
2023-10-30 09:08:55 +0300

    Create the form fields for Signup

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -16,4 +16,37 @@ const Signup=(e)=>{
         //Link to the database.
     }
 
+    return(
+        <div className="popup">
+            <div className="pop-content">
+                <h2>Signup</h2>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    name="Username"
+                    value={formdata.username}
+                    onChange={handleChange}
+                    placeholder="Username"
+                    />
+                    <input
+                    type="email"
+                    name="Email"
+                    value={formdata.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                    />
+                    <input
+                    type="password"
+                    name="Password"
+                    value={formdata.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                    />
+                    <button type="submit">SignUp</button>
+                </form>
+            </div>
+        </div>
+    )
+
 }
+export default Signup;

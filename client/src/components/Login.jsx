47f003a259850f4d4eeda81c246b8b43624787e6
Noah <noah.ntabo@student.moringaschool.com>
2023-10-30 08:24:04 +0300

    Create login function

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,2 +1,11 @@
 import React,{useState} from "react";
 import "./Login.css"
+
+
+
+const Login=()=>{
+    const [formdata,setFormdata]=useState({
+        username:'',
+        password:'',
+    });
+}

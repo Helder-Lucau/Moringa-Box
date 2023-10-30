4b051750414198d5648cfec52e38f287c78627b9
Noah <noah.ntabo@student.moringaschool.com>
2023-10-30 08:53:55 +0300

    Create Signup function and initialize useState

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,1 +1,14 @@
 import React,{useState} from "react";
+import './Signup.css'
+
+const Signup=(e)=>{
+    const[formdata,setFormdata]=useState({
+        username:'',
+        email:'',
+        password:'',
+    });
+
+    const handleChange=(e)=>{
+        
+    }
+}

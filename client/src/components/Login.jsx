c4e1f4f03ee510f7326ea8e64e0c50e723d9a367
Noah <noah.ntabo@student.moringaschool.com>
2023-10-31 16:00:04 +0300

    Make the forms responsive

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,64 +1,66 @@
-import React,{useState} from "react";
-import "./Login.css"
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Login.css";
 
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log("Event:", e);
+  };
 
-
-const Login=()=>{
-    const [formdata,setFormdata]=useState({
-        email:'',
-        password:'',
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+    alert("Welcome back!");
+    setFormData({
+        name: '',
+        email: '',
+        password: '',
     });
+  };
 
-    const handleChange=(e)=>{
-        setFormdata({...formdata,[e.target.name]:e.target.value});
-        console.log("Event:", e);
-    };
-
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log("Form Submitted");//prevent reloading of page once input fields are filled
-
-        //Link to the database.
-    }
-
-    return(
+  return (
+    <div className="popup">
+      <div className="popup-content">
         <div className="login-popup">
-            <div className="login-popup-content">
-                <h2>M-<span>Box</span></h2>
-                <h5>Login To Your Account</h5>
-                <form onSubmit={handleSubmit}>
-
-                    <label>
-                        Email:
-                        <input
-                            type="email"
-                            name="Email"
-                            value={formdata.email}
-                            onChange={handleChange}
-                            
-                        />
-                    </label>
-                    <label>
-                        Password:
-                        <input
-                            type="password"
-                            name="Password"
-                            value={formdata.password}
-                            onChange={handleChange}
-                            
-                        />
-                    </label>
-                    
-                    
-                    
-                    <button type="submit">Login</button>
-
-                    <p>Don't have an account?<Link to="/Signup">Sign Up</Link></p>
-                </form>
-            </div>
+          <div className="login-popup-content">
+            <h2>M-<span>Box</span></h2>
+            <h5>Login To Your Account</h5>
+            <form onSubmit={handleLogin}>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <button type="submit">Login</button>
+              <p>
+                Don't have an account? <a href="/Signup">Sign Up</a>
+              </p>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
 export default Login;
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,84 +1,57 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './Signup.css';
 import { Link } from 'react-router-dom';
 
-const Signup=()=>{
-    const[formData,setFormData]=useState({
-        firstname:'',
-        lastname:'',
-        email:'',
-        password:'',
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
     });
 
-    const handleChange=(e)=>{
-        
-        setFormData({...formData,[e.target.name]:e.target.value});
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
         console.log("Event:", e);
     };
 
-    const handleSubmit=(e)=>{
+    const handleSignup = (e) => {
         e.preventDefault();
         console.log("Form Submitted");
-        
+        alert('Account created successfully!')
+        setFormData({
+            name: '',
+            email: '',
+            password: '',
+        });
     };
 
-
-    return(
-        <div className="signup-popup">
-            <div className="signup-popup-content">
-                <h2>Create an M-<span>Box</span> Account</h2>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        FirstName:
-                        <input
-                        type="text"
-                        name="FirstName"
-                        value={formData.firstname}
-                        onChange={handleChange}
-                        
-                    />
-                    </label>
-                    <label>
-                        LastName:
-                        <input
-                            type="text"
-                            name="LastName"
-                            value={formData.lastname}
-                            onChange={handleChange}
-                            
-                    />
-                    </label>
-                    <label>
-                        Email:
-                        <input
-                            type="email"
-                            name="Email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            
-                    />
-                    </label>
-                    <label>
-                        Password:
-                        <input
-                        type="password"
-                        name="Password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        
-                    />
-                    </label>
-                    
-                   
-                    
-                    
-                    <button type="submit">Sign Up</button>
-
-                    <p>Already have an account?<Link to="/Login">Login</Link></p>
-                </form>
+    return (
+        <div className="popup">
+            <div className="popup-content">
+                <div className="signup-popup">
+                    <div className="signup-popup-content">
+                        <h2>Create an M-<span>Box</span> Account</h2>
+                        <form onSubmit={handleSignup}>
+                            <label>
+                                Name:
+                                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+                            </label>
+                            <label>
+                                Email:
+                                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                            </label>
+                            <label>
+                                Password:
+                                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                            </label>
+                            <button type="submit">Sign Up</button>
+                            <p>Already have an account? <Link to="/login">Login</Link></p>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Signup;

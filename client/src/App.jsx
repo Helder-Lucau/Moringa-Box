7137133007d2759c2f7d7b86514b8f7313212d7c
Helder-Lucau <helderlucau@gmail.com>
2023-11-01 15:41:33 +0300

    debugging dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react"
+import { BrowserRouter as Router, Routes } from "react-router-dom"
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Routes path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 

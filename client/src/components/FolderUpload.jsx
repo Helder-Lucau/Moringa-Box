ca1c1f15059c52746f721174e278673859b5b3d5
Noah <noah.ntabo@student.moringaschool.com>
2023-11-02 12:25:03 +0300

    Create structure of Folder upload file

---
+++ client/src/components/FolderUpload.jsx
@@ -0,0 +1,16 @@
+//Form to upload files.
+import React from "react";
+
+const FolderUpload=()=>{
+
+    const handleFileUpload=(e)=>{
+//Implement logic to handle file upload
+    }
+
+
+    return(
+        <input type="file" onChange={handleFileUpload}/>
+
+    );
+};
+export default FolderUpload;

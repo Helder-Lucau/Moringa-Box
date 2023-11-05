97c698b5deab593563c5d21a460871a451447d2e
Noah <noah.ntabo@student.moringaschool.com>
2023-11-05 22:39:34 +0300

    Styling

--- client/src/components/UserHeader.jsx
+++ client/src/components/UserHeader.jsx
@@ -1,12 +1,16 @@
 //The specific header of the user that will display in dashboard
 import React from "react";
 import {Link} from "react-router-dom"
+import "./UserHeader.css"
+import logosimage from "../assets/logo.png";
 
 
 const UserHeader=()=>{
 return(
     <div className="header">
-        <div className="logo">Logo</div>
+        <div className="logo">
+            <img src={logosimage} alt="Logo"/>
+        </div>
         <div className="user-info">Username</div>
         <Link to='/' className="logout-button">Logout</Link>
     </div>

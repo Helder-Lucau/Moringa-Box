d0a49343d9a1d3fda427a2c8fe2c68a8b0793e6e
Helder-Lucau <helderlucau@gmail.com>
2023-11-08 10:43:57 +0300

    styling sign up page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,6 +4,7 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import Swal from "sweetalert2";
 import "./Signup.css";
+import Navbar from "./Navbar";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -42,10 +43,10 @@ const Signup = () => {
 
           Swal.fire({
             title: "Success!",
-            text: "M-Box account created successfully!",
+            text: "Account created successfully!",
             icon: "success",
-            confirmButtonText: "Nice",
-            confirmButtonColor: "#f1cc17",
+            confirmButtonText: "Continue",
+            confirmButtonColor: "#ba324f",
           });
 
           navigate("/login");
@@ -59,71 +60,82 @@ const Signup = () => {
   });
 
   return (
-    <div className="popup">
-      <div className="popup-content">
-        <div className="signup-popup">
-          <div className="signup-popup-content">
-            <h2>Create an M-<span>Box</span> Account</h2>
-            <form onSubmit={formik.handleSubmit}>
-              <label>
-                First Name:
-                <input
-                  type="text"
-                  name="firstname"
-                  value={formik.values.firstname}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </label>
-              {formik.touched.firstname && formik.errors.firstname ? (
-                <div style={{ color: "red" }}>{formik.errors.firstname}</div>
-              ) : null}
-              <label>
-                Last Name:
-                <input
-                  type="text"
-                  name="lastname"
-                  value={formik.values.lastname}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </label>
-              {formik.touched.lastname && formik.errors.lastname ? (
-                <div style={{ color: "red" }}>{formik.errors.lastname}</div>
-              ) : null}
-              <label>
-                Email:
-                <input
-                  type="email"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </label>
-              {formik.touched.email && formik.errors.email ? (
-                <div style={{ color: "red" }}>{formik.errors.email}</div>
-              ) : null}
-              <label>
-                Password:
-                <input
-                  type="password"
-                  name="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </label>
-              {formik.touched.password && formik.errors.password ? (
-                <div style={{ color: "red" }}>{formik.errors.password}</div>
-              ) : null}
-              <button type="submit">Sign Up</button>
-              <p>
-                Already have an account? <Link to="/login">Login here</Link>
-              </p>
-            </form>
+    <div>
+      <Navbar />
+      <div className="flex justify-center items-center w-full mx-auto p-10">
+        <form onSubmit={formik.handleSubmit} className="text-white max-w-[400px] rounded-xl w-full mx-auto bg-[#1e232a] p-8">
+          <h2 className="text-2xl font-medium text-center py-4">Create an M-<span className="text-red-600">BOX</span> Account</h2>
+          <div className="flex flex-col mb-4">
+            <label>
+              First Name
+            </label>
+            <input
+              type="text"
+              name="firstname"
+              value={formik.values.firstname}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className="p-2 text-black bg-gray-200"
+            />
+
+            {formik.touched.firstname && formik.errors.firstname ? (
+              <div style={{ color: "red" }}>{formik.errors.firstname}</div>
+            ) : null}
           </div>
-        </div>
+          <div className="flex flex-col mb-4">
+            <label>
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="lastname"
+              value={formik.values.lastname}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className="p-2 text-black bg-gray-200"
+            />
+
+            {formik.touched.lastname && formik.errors.lastname ? (
+              <div style={{ color: "red" }}>{formik.errors.lastname}</div>
+            ) : null}
+          </div>
+          <div className="flex flex-col mb-4">
+            <label>
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className="p-2 text-black bg-gray-200"
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div style={{ color: "red" }}>{formik.errors.email}</div>
+            ) : null}
+          </div>
+          <div className="flex flex-col mb-4">
+            <label>
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              className="p-2 text-black bg-gray-200"
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div style={{ color: "red" }}>{formik.errors.password}</div>
+            ) : null}
+          </div>
+          <button type="submit" className="w-full rounded-2xl py-2 mt-6 bg-[#ba324f] hover:bg-[#9eb3c2]">SIGN UP</button>
+          <p className="text-center mt-8">
+            Already have an account? <Link to="/login">Log in</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
